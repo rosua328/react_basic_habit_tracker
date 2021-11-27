1c9473d9aa10f358dd5f57eb8b424683bbba897c
수호 <rosua328@gmail.com>
2021-11-28 08:10:51 +0900

    add select and video scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
 import styles from './App.module.css';
 
-import React, { Component, useEffect, useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import VideoList from './Component/video_list/video_list';
 import SearchHeader from './Component/search_header/search_header';
+import VideoDetail from './Component/video_detail/video_detail';
 
 
-function App({youtube}){
-  const [videos, setvideos] = useState([]);
-const search = query =>{
-  youtube
-  .search(query)
-  .then(videos => setvideos(videos));
-};
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  useEffect(()=>{
+  const selectVideo = video => {
+    setSelectedVideo(video);
+  };
+
+  const search = query => {
     youtube
-    .mostPopular()
-    .then(videos => setvideos(videos));
+      .search(query) 
+      .then(videos => setVideos(videos));
+  };
+
+  useEffect(() => {
+    youtube
+      .mostPopular()
+      .then(videos => setVideos(videos));
   }, []);
 
-  return(
-  <div className={styles.app}>
-  <SearchHeader onSearch={search}/>
-  <VideoList videos={videos}/>
-  </div>
+  return (
+    <div className={styles.app}>
+     <SearchHeader onSearch={search}/>
+     <section className={styles.content}>
+       {selectedVideo&&
+       <div className={styles.detail}>
+       <VideoDetail video={selectedVideo}/>
+       </div>}
+       <div className={styles.list}>
+       <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo?'list': 'grid'}/>
+       </div>
+     </section>
+    </div>
   );
 }
 
